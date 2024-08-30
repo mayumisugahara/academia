d6edfdb98934da927e0f6fdfe9de95a4e06d26a6
mayumisugahara <106741982+mayumisugahara@users.noreply.github.com>
2024-08-29 21:29:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,168 @@
+export const perguntas = [
+    {
+        enunciado: "Qual tecnologia de IA é mais eficaz para monitorar o progresso no treinamento físico",
+        alternativas: [
+            {
+                texto: "Aplicativos de treino baseados em IA",
+                afirmacao: [
+                    "Podem analisar dados em tempo real e ajustar o plano de treino de acordo com o desempenho.",
+                    "Fornecem feedback personalizado para melhorar a técnica e evitar lesões.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: " Dispositivos vestíveis com IA ",
+                afirmacao: [
+                    "Monitoram métricas de saúde, como frequência cardíaca e calorias queimadas, durante o treino.",
+                    "Oferecem insights sobre a recuperação e sugerem quando é o melhor momento para o próximo treino.",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual IA é mais útil para personalizar planos de nutrição de acordo com o treino?",
+        alternativas: [
+            {
+                texto: " Plataformas de IA que analisam a dieta",
+                afirmacao: [
+                    "Podem sugerir ajustes nutricionais com base nas necessidades energéticas do treino.",
+                    "Adaptam as recomendações nutricionais conforme o progresso e metas do usuário.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Aplicativos de monitoramento de calorias com IA",
+                afirmacao: [
+                    "Ajudam a controlar a ingestão calórica para alcançar objetivos específicos, como ganho de massa muscular ou perda de gordura.",
+                    " Podem recomendar alimentos com base nas preferências e restrições alimentares do usuário..",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual IA é mais eficaz na correção de postura durante exercícios?",
+        alternativas: [
+            {
+                texto: "Câmeras de monitoramento com IA",
+                afirmacao: [
+                    "Capturam a forma do usuário e oferecem correções em tempo real para melhorar a postura.",
+                    "Podem identificar desequilíbrios musculares e sugerir exercícios corretivos.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Sensores de movimento com IA",
+                afirmacao: [
+                    " Monitoram os movimentos do corpo e fornecem feedback imediato sobre a execução dos exercícios..",
+                    " Ajudam a prevenir lesões ao detectar padrões de movimento incorretos..",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual tecnologia de IA é mais vantajosa para otimizar o tempo de recuperação pós-treino?",
+        alternativas: [
+            {
+                texto: " Aplicativos de IA que analisam dados de sono",
+                afirmacao: [
+                    "Sugerem a quantidade ideal de sono com base no desgaste físico do treino ",
+                    "Podem recomendar ajustes no horário de dormir para otimizar a recuperação muscular ",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Dispositivos de IA que monitoram a variabilidade da frequência cardíaca (VFC)",
+                afirmacao: [
+                    " Avaliam o nível de estresse do corpo e sugerem quando é o momento ideal para treinar novamente.   .",
+                     " Oferecem dicas personalizadas para melhorar a recuperação com base nos dados coletados..",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual IA é mais eficaz para motivar os usuários a manterem a consistência nos treinos?",
+        alternativas: [
+            {
+                texto: " Assistentes virtuais de IA",
+                afirmacao: [
+                    " Enviam lembretes diários e mensagens motivacionais para incentivar a regularidade nos treinos.",
+                    "  Podem criar desafios personalizados para manter o usuário engajado.",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Comunidades de treino baseadas em IA ",
+                afirmacao: [
+                    "Conectam usuários com interesses similares, criando uma rede de apoio e motivação.",
+                    "Oferecem comparações de desempenho e rankings para incentivar a competição saudável.",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual IA é mais útil para otimizar o treino cardiovascular?",
+        alternativas: [
+            {
+                texto: " Algoritmos de IA para análise de desempenho",
+                afirmacao: [
+                    "Ajustam automaticamente a intensidade do treino com base na frequência cardíaca em tempo real. ",
+                    "Podem criar intervalos personalizados para maximizar a queima de calorias.",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Plataformas de IA que analisam dados de corrida",
+                afirmacao: [
+                    " Avaliam a técnica de corrida e sugerem ajustes para melhorar a eficiência.",
+                    " Podem recomendar alterações na cadência e postura para reduzir o risco de lesões.",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual IA é mais eficiente para treinos de força?",
+        alternativas: [
+            {
+                texto: " Aplicativos de IA que registram pesos e repetições",
+                afirmacao: [
+                    " Analisam o desempenho ao longo do tempo e sugerem progressões de carga ",
+                    " Podem ajustar automaticamente o número de séries e repetições para otimizar o ganho de força.",
+                ],
+                proxima: 8,
+            },
+            {
+                texto: " Dispositivos de IA para levantamento de peso ",
+                afirmacao: [
+                    " Monitoram a velocidade e a forma durante os levantamentos para garantir segurança e eficácia..",
+                    " Oferecem feedback em tempo real para corrigir a postura e evitar lesões..",
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual IA é mais indicada para treinos funcionais?",
+        alternativas: [
+            {
+                texto: " Sensores de IA para análise de movimento  ",
+                afirmacao: [
+                    "Avaliam a eficiência dos movimentos funcionais e sugerem melhorias na técnica.",
+                    " Podem identificar fraquezas musculares e recomendar exercícios específicos para fortalecer essas áreas. ",
+                ],
+            },
+            {
+                texto: "Câmeras de IA para treino funcional ",
+                afirmacao: [    
+                    "Analisam a execução dos exercícios e corrigem a postura para garantir a segurança.",
+                    "Podem adaptar o treino conforme o progresso do usuário, tornando-o mais desafiador à medida que o desempenho melhora.",
+                ],
+            },
+        ]
+    }
+];
